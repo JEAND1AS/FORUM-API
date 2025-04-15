8590be4b6fd63b7138a2fbbbf834872833e2cf91
Jean Dias <83466660+JEAND1AS@users.noreply.github.com>
2025-04-15 17:56:50 +0000

    Configuração do prisma, bacno de dados e usuários

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, DatabaseModule],
   providers: [UserService],
 })
 export class AppModule {}
